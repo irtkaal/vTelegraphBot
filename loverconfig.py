9bd56de2bdef054854768c21ffe30c7467260188
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2023-01-23 08:48:02 +0530

    Update loverconfig.py

--- loverconfig.py
+++ loverconfig.py
@@ -8,7 +8,7 @@ from telegraph import upload_file
 import os
 
 teletips=Client(
-    "MediaToTelegraphLink",
+    "Lover-MediaToTelegraphLink",
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"],
     bot_token = os.environ["BOT_TOKEN"]
